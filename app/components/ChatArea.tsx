2507a6ce4a6bf87748b2e11a525fa2cda84a5819
Abi-de-jo <abisheikabisheik102@gmail.com>
2026-05-03 21:47:12 +0530

    fix env case

--- app/components/ChatArea.tsx
+++ app/components/ChatArea.tsx
@@ -164,7 +164,7 @@ export default function ChatArea({ onOpenDocs }: ChatAreaProps) {
 
   return (
     <>
-      <div className="flex-1 overflow-y-auto no-scrollbar">
+      <div className="flex-1 overflow-y-auto mt-14 no-scrollbar">
         <div className="min-h-full">
           {showWelcome ? (
             <WelcomeScreen onSendMessage={sendMessage} />
